4a3165e7cba24e611431c229c72dfdb5e226809f
shekhar <shekharjha7417870578@gmail.com>
2022-08-22 12:19:23 +0530

    Added uc1 : creating stack using LinkedList

---
+++ src/com/bridgelabz/stackandqueues/Stack.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.stackandqueues;
+
+public class Stack {
+    public static void main(String[] args) {
+        StackUsingLinkedList stack = new StackUsingLinkedList();
+
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+
+
+
+        stack.display();
+
+    }
+}
